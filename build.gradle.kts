488e5af6ce5137f44236b5a5be0caeaa045ba0b4
Jarimatti Valkonen <jarimatti@me.com>
2020-06-24 22:17:49 +0100

    Update dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    scala
+    java
 }
 
 group = "fi.jarimatti"
@@ -12,17 +12,17 @@ repositories {
 
 dependencies {
     implementation("org.erlang.otp", "jinterface", "1.6.1")
-    implementation("io.netty", "netty-all", "4.1.33.Final")
-    implementation("org.apache.logging.log4j", "log4j-api", "2.11.1")
-    implementation("org.apache.logging.log4j", "log4j-api-scala_2.12", "11.0")
-    
-    implementation("org.apache.logging.log4j", "log4j-core", "2.11.1")
-    implementation("com.fasterxml.jackson.core", "jackson-databind", "2.9.+")
-    implementation("com.fasterxml.jackson.dataformat", "jackson-dataformat-yaml", "2.9.+")
+    implementation("io.netty", "netty-all", "4.1.50.Final")
+    implementation("org.apache.logging.log4j", "log4j-api", "2.13.3")
+    implementation("org.apache.logging.log4j", "log4j-core", "2.13.3")
 
-    testCompile("junit", "junit", "4.12")
+    testImplementation("org.junit.jupiter", "junit-jupiter", "5.6.2")
 }
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
